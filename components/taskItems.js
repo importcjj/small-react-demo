e6e2cdc096f4b460d15326574ba170f5f5f6e92d
jiaju.chen <619507631@qq.com>
2015-12-04 19:40:04 +0800

    Update taskItems.js

--- components/taskItems.js
+++ components/taskItems.js
@@ -7,7 +7,6 @@ var TaskItem = React.createClass({
         )
     },
     render: function () {
-        console.log(this.props);
         var task = this.props.task;
         return (
             <li>{task}
